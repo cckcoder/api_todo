21a237a8686630d4718af3f36601156f3e728d17
codewizz <c.chaiwat.ch@gmail.com>
2022-09-02 22:43:08 +0700

    complete logout user

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::resource('todo', TodoController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
