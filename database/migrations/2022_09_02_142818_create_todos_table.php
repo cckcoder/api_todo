60ba715be6325382d7fd31c89a2470abbf73fec9
codewizz <c.chaiwat.ch@gmail.com>
2022-09-02 21:36:22 +0700

    migrage todo table

--- database/migrations/2022_09_02_142818_create_todos_table.php
+++ database/migrations/2022_09_02_142818_create_todos_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('activity');
+            $table->boolean('is_complete', false);
             $table->timestamps();
         });
     }
